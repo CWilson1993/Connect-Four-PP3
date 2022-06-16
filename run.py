5379cd2c31cffd0747f820e942cb55198bad8e55
CWilson1993 <cian.wilson10@gmail.com>
2022-06-16 21:41:35 +0000

    Updating code for best practices

--- run.py
+++ run.py
@@ -1,45 +1,47 @@
 from random import randint
 
+
 # Class stack (for each column)
 class Stack:
     def __init__(self):
         self._list = []
-    
+
     def __len__(self):
-        return len(self._list)    
-    
+        return len(self._list)
+
     def push(self, element):
         if len(self._list) <= 6:
             self._list.append(element)
         else:
             return
-    
+
     def peek(self):
         return self._list[-1]
 
 
 # Below is the code to initilaize the board which will be empty at first
 def initialBoard():
-    rows = ['a','b','c','d','e','f']
+    rows = ['a', 'b', 'c', 'd', 'e', 'f']
     board = []
-    for i in range(0,len(rows)):
+    for i in range(0, len(rows)):
         board.append([' '] * 7)
-    
+
     return board
 
 
-# Below is the empty initial stacks, the board will require 7 stacks(columns) to play connect four
+# Below is the empty initial stacks,
+# the board will require 7 stacks(columns) to play connect four
 def initialStacks():
-    S = [ Stack(), Stack(), Stack(), 
-         Stack(), Stack(), Stack(), Stack() ]
+    S = [Stack(), Stack(), Stack(),
+         Stack(), Stack(), Stack(), Stack()]
     return S
 
 
 # The below prints the game board
 def printBoard(board):
-    rows = ['a','b','c','d','e','f']
+    rows = ['a', 'b', 'c', 'd', 'e', 'f']
     top = '    1   2   3   4   5   6   7   '
-    row = [[n] for n in range(0,7)]
+    row = [[n] for n in range(0, 7)]
     row[0][0] = 'f | '
     row[1][0] = 'e | '
     row[2][0] = 'd | '
@@ -48,19 +50,21 @@ def printBoard(board):
     row[5][0] = 'a | '
     print('')
     print('  ' + '-'*(len(top)-3))
-    for j in range(0,len(rows)):
-        for i in range(1,8):
+    for j in range(0, len(rows)):
+        for i in range(1, 8):
             row[j][0] = row[j][0] + str(board[j][i-1]) + ' | '
         print(row[j][0])
         print('  ' + '-'*((len(row[j][0])-3)))
     print(top)
     print('')
 
-# The below code makes a random choice for the computer and prompts the player to make a move, giving parameters for the choice
+
+# The below code makes a random choice for the computer
+# and prompts the player to make a move, giving parameters for the choice
 def move(piece, board, Stacks, computer):
-    Set0 = {'1','2','3','4','5','6','7'}
+    Set0 = {'1', '2', '3', '4', '5', '6', '7'}
     if piece == computer:
-        pos = randint(1,7)
+        pos = randint(1, 7)
         if len(Stacks[pos-1]) < 6:
             Stacks[pos-1].push(piece)
             board[6-len(Stacks[pos-1])][pos-1] = \
@@ -69,10 +73,10 @@ def move(piece, board, Stacks, computer):
             move(piece, board, Stacks, computer)
     else:
         pos = str(input('Your move:\n '))
-        if (pos in Set0) == False:
+        if (pos in Set0) is False:
             print('Input must be integer between 1 and 7')
             move(piece, board, Stacks, computer)
-        else: 
+        else:
             pos = int(pos)
             if len(Stacks[pos-1]) < 6:
                 Stacks[pos-1].push(piece)
@@ -85,46 +89,47 @@ def move(piece, board, Stacks, computer):
 
 
 # The below code checks the winner from all different angles
-def checkWin(S,board):
-    game = False   
+def checkWin(S, board):
+    game = False
     # Checks the horizontal board for a winner
-    for j in range(0,6):
-        for i in range(3,7):
-            if (board[j][i]==board[j][i-1]==\
-                board[j][i-2]==board[j][i-3]==S):
-                    game = True
+    for j in range(0, 6):
+        for i in range(3, 7):
+            if (board[j][i] == board[j][i-1] == board[j][i-2] ==
+                    board[j][i-3] == S):
+                game = True
             else:
                 continue
     # Checks the vertical board for a winner
-    for i in range(0,7):
-        for j in range(3,6):
-            if (board[j][i]==board[j-1][i]==\
-                board[j-2][i]==board[j-3][i]==S):
-                    game = True
+    for i in range(0, 7):
+        for j in range(3, 6):
+            if (board[j][i] == board[j-1][i] ==
+                    board[j-2][i] == board[j-3][i] == S):
+                game = True
             else:
                 continue
     # Checks the diagonals of the board for a winner
-    for i in range(0,4):
-        for j in range(0,3):
-            if (board[j][i]==board[j+1][i+1]==\
-                board[j+2][i+2]==board[j+3][i+3]==S or
-                board[j+3][i]==board[j+2][i+1]==\
-                board[j+1][i+2]==board[j][i+3]==S):
-                    game = True
+    for i in range(0, 4):
+        for j in range(0, 3):
+            if (board[j][i] == board[j+1][i+1] ==
+                    board[j+2][i+2] == board[j+3][i+3] == S or
+                    board[j+3][i] == board[j+2][i+1] ==
+                    board[j+1][i+2] == board[j][i+3] == S):
+                game = True
             else:
                 continue
-    if game == True:
+    if game is True:
         print(S + ' wins!')
     return game
 
 
-# Below is the main program, it includes the initial prompt for the user to choice and identifier
+# Below is the main program,
+# it includes the initial prompt for the user to choice and identifier
 
 def main():
     # Below will prompt the user to chose X or O
-    player1 = str( input('Your choice X or O:\n ') )
+    player1 = str(input('Your choice X or O:\n '))
     if player1 != 'X' and player1 != 'O':
-        player1 = str( input('Your choice X or O:\n ') )
+        player1 = str(input('Your choice X or O:\n '))
     if player1 == 'X':
         computer1 = 'O'
     else:
@@ -133,26 +138,27 @@ def main():
     board = initialBoard()
     Stacks = initialStacks()
     printBoard(board)
-    print('How to play: Enter an integer between 1 and 7 ' + \
-          'which relates to each column on the board. ' + \
-          'The first to stack 4 pieces next to each other, ' + \
+    print('How to play: Enter an integer between 1 and 7 ' +
+          'which relates to each column on the board. ' +
+          'The first to stack 4 pieces next to each other, ' +
           'either horizontally, vertically or diagonally wins.')
     game = False
-    while game == False:
+    while game is False:
         # Player who chose X
-        board, Stacks = move('X',board,Stacks,computer1)
+        board, Stacks = move('X', board, Stacks, computer1)
         printBoard(board)
-        game = checkWin('X',board)
-        if game == True:
+        game = checkWin('X', board)
+        if game is True:
             break
 
         # Player who chose O
-        board, Stacks = move('O',board,Stacks,computer1)
+        board, Stacks = move('O', board, Stacks, computer1)
         printBoard(board)
-        game = checkWin('O',board)
-        if game == True:
+        game = checkWin('O', board)
+        if game is True:
             break
     print('Well played.')
+
 
 if __name__ == '__main__':
     main()
